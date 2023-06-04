537b580cc058370a64b4d491141267a681b7f516
KatsumiYamashita <qwerty681104@gmail.com>
2023-06-04 13:54:02 +0900

    最初のコミット

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import main
+
+st.title("React: A + B → Y")
+
+def reactant_A():
+
+    reactant_A: str = st.text_input("Enter reactant A")
+
+    if reactant_A:
+        st.write(f"reactant A is :{reactant_A}")
+    else:
+        st.write("Please enter reactant A.")
+    return reactant_A
+
+def reactant_B():
+
+    reactant_B: str = st.text_input("Enter reactant B")
+
+    if reactant_B:
+        st.write(f"reactant B is :{reactant_B}")
+    else:
+        st.write("Please enter reactant B.")
+    return reactant_B
+
+reactant_A2 = reactant_A()
+reactant_B2 = reactant_B()
+
+product_Y = main.react_ai(reactant_A2, reactant_B2)
+st.write(product_Y)
