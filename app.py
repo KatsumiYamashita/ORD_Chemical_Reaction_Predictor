b0dcd4839d0d8d787a3ea0e1804f87a754293220
KatsumiYamashita <qwerty681104@gmail.com>
2023-06-09 18:58:36 +0900

    最新版に更新する

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # モジュールのインポート
 import streamlit as st
 import main
+import openai
 
 from rdkit import Chem
 from rdkit.Chem import Draw, rdChemReactions as Reactions
@@ -33,34 +34,23 @@ def enter_reactant_B():
         st.write("Please enter reactant B.")
     return reactant_B
 
-# 化合物ABを変数に格納
-entered_reactant_A = enter_reactant_A()
-entered_reactant_B = enter_reactant_B()
-
 # 反応物Yを探索する関数を定義
 def react_AB():
 
     if entered_reactant_A and entered_reactant_B:
-        generated_product_Y = main.get_prodY_SMILES(entered_reactant_A, \
-                                        entered_reactant_B, \
-                                        training_dataset)
+        generated_product_Y = main.get_prodY_SMILES(entered_reactant_A,\
+                                                    entered_reactant_B,\
+                                                    training_dataset)
         st.write(generated_product_Y)
         #product_Y = gpt のモデルによって回答の形式が異なる. SMILESだけ取得されるわけではないから難しい
     else:
         st.write("Waiting...")
     return generated_product_Y
 
-product_Y = react_AB()
-
-# 化合物ABYをSMILESからMolオブジェクトへ変換
-#mol_A = Chem.MolFromSmiles(reactant_A2)
-#mol_B = Chem.MolFromSmiles(reactant_B2)
-#mol_Y = Chem.MolFromSmiles(product_Y2)
-
 # molオブジェクトからimageを生成
 def show_rxn_formula(A, B, Y):
 
-    drawer = rdMolDraw2D.MolDraw2DSVG(660,350)
+    drawer = rdMolDraw2D.MolDraw2DSVG(660,300)
     rxn = Reactions.ReactionFromSmarts(f'{A}.{B}>>{Y}',\
                                         useSmiles=True)
     drawer.DrawReaction(rxn)
@@ -71,12 +61,17 @@ def show_rxn_formula(A, B, Y):
 
     return show_rxn_formula
 
-if entered_reactant_A and entered_reactant_B and product_Y:
+#api_keyの入力
+api_key = st.text_input("API keyを入力してください")
+openai.api_key = api_key
+
+# 化合物ABを変数に格納
+if api_key:
+    entered_reactant_A = enter_reactant_A()
+    entered_reactant_B = enter_reactant_B()
+
+if entered_reactant_A and entered_reactant_B:
+    product_Y = react_AB()
+
+#if entered_reactant_A and entered_reactant_B and product_Y:
     show_rxn_formula(entered_reactant_A, entered_reactant_B, product_Y)
-   
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import openai
-import os
 
-openai.api_key_path = "./openai_api_key.txt"
+def get_prodY_SMILES(A: str, B: str, training_dataset):
 
-def get_prodY_SMILES(A: str, B: str, context):
-
-    context =f"{context}\
+    context =f"{training_dataset}\
             test data:\
             {A} + {B} = '?'\
              "
@@ -19,8 +16,3 @@ def get_prodY_SMILES(A: str, B: str, context):
     
     product_Y = response.choices[0].text.strip()
     return product_Y
-    
-#sub_A = "CC1=NN(C=C1NC2=NC=C(C(=C2)I)C(F)(F)F)C"
-#sub_B = "CONC(=O)C1=CC=CC=C1N"
-
-#react_ai(sub_A, sub_B)
