4d018eead71e60487eaaac05f9495893428d11ca
KatsumiYamashita <qwerty681104@gmail.com>
2023-06-18 17:00:49 +0900

    データセットを入力されたテストデータを基に選別する

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # モジュールのインポート
+import extract
 import main
 import openai
 import pubchempy as pcp
+import pandas as pd
 import streamlit as st
 
-from context import training_dataset
 from streamlit_ketcher import st_ketcher
+from rdkit import rdBase, Chem, DataStructs
+from rdkit.Avalon import pyAvalonTools
+from rdkit.Chem import AllChem, Draw, rdMHFPFingerprint
+from rdkit.Chem.Fingerprints import FingerprintMols
 
 # 化合物A,Bを入力する関数の定義
 def enter_reactants():
@@ -13,32 +18,19 @@ def enter_reactants():
     DEFAULT_A = r"CC1=NN(C=C1NC2=NC=C(C(=C2)I)C(F)(F)F)C"
     DEFAULT_B = r"CONC(=O)C1=CC=CC=C1N"
 
-    col_A, col_B = st.columns(2)
+    col_A, col_B = st.columns(2, gap="large")
 
     with col_A:
         entered_A = st.text_input("Enter reactant A 'SMILES'", DEFAULT_A)
-        reactant_A = st_ketcher(entered_A)
-        st.markdown(f"reactant A is ``{reactant_A}``")
+        reactant_A_smiles = st_ketcher(entered_A)
+        reactant_A_mol = Chem.MolFromSmiles(reactant_A_smiles)
 
     with col_B:
         entered_B = st.text_input("Enter reactant B 'SMILES'", DEFAULT_B)
-        reactant_B = st_ketcher(entered_B)
-        st.markdown(f"reactant B is: ``{reactant_B}``")
+        reactant_B_smiles = st_ketcher(entered_B)
+        reactant_B_mol = Chem.MolFromSmiles(reactant_B_smiles)
 
-    return reactant_A, reactant_B
-
-# 反応物Yを探索する関数を定義
-def react_AB():
-
-    if entered_reactant_A and entered_reactant_B:
-        generated_product_Y = main.get_prodY_SMILES(entered_reactant_A,\
-                                                    entered_reactant_B,\
-                                                    training_dataset)
-        
-        #product_Y = gpt のモデルによって回答の形式が異なる. SMILESだけ取得されるわけではないから難しい
-    else:
-        st.write("Waiting...")
-    return generated_product_Y
+    return reactant_A_smiles, reactant_B_smiles, reactant_A_mol, reactant_B_mol
 
 # 反応式を表示させる関数を定義
 def show_rxn_formula(A, B, Y):
@@ -52,21 +44,53 @@ def show_rxn_formula(A, B, Y):
 st.set_page_config(layout="wide")
 st.title("React: A + B → Y")
 
-#api_keyの入力
+# データセットのインポート
+ord_dataset_id = "ord_dataset-00005539a1e04c809a9a78647bea649c"
+
+df_smiles_ABY,\
+df_mol_ABY,\
+df_smiles_mol\
+= extract.import_dataset(ord_dataset_id)
+
+# df_molの各要素に対して maccs_fps を生成
+df_maccs_fps_ABY,\
+df_smiles_maccs_fps\
+= extract.generate_maccs_fps_df(df_mol_ABY, df_smiles_ABY)
+
+# api_keyの入力
 api_key = st.text_input("API keyを入力してください", type='password')
 openai.api_key = api_key
 
-# 化合物ABを変数に格納(関数の呼び出し)
-entered_reactant_A, entered_reactant_B = enter_reactants()
+# テスト化合物ABを変数に格納(関数の呼び出し)
+reactant_A_smiles,\
+reactant_B_smiles,\
+reactant_A_mol, \
+reactant_B_mol \
+= enter_reactants()
+
+# テスト化合物ABのmaccs fpsを生成
+reactant_A_maccs_fps = AllChem.GetMACCSKeysFingerprint(reactant_A_mol)
+reactant_B_maccs_fps = AllChem.GetMACCSKeysFingerprint(reactant_B_mol)
+
+# トレーニングデータの取得
+training_dataset_rank_tnmt_A\
+= extract.extract_training_data(df_smiles_maccs_fps,\
+                                df_maccs_fps_ABY,\
+                                reactant_A_maccs_fps,\
+                                reactant_B_maccs_fps,\
+                                35)
 
 # 化合物ABを反応させる (Yを予測させる)
 if api_key:
-    generated_product_Y = react_AB()
+    generated_product_Y \
+    = main.get_prodY_SMILES(reactant_A_smiles, \
+                            reactant_B_smiles, \
+                            training_dataset_rank_tnmt_A)
+    if generated_product_Y:
+        show_rxn_formula(reactant_A_smiles, \
+                        reactant_B_smiles, \
+                        generated_product_Y) 
+
 else:
     pass
 
-# A, B, およびYの反応式を表示させる
-if entered_reactant_A and entered_reactant_B and generated_product_Y:
-    show_rxn_formula(entered_reactant_A, entered_reactant_B, generated_product_Y)    
-else:
-    pass
