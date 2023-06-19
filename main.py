cae97a9154f4e918758de4c58711445700ebf629
KatsumiYamashita <qwerty681104@gmail.com>
2023-06-19 20:58:13 +0900

    プロンプトを改善する

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import openai
 import re
 
-def get_prodY_SMILES(A: str, B: str, training_dataset):
+def get_prodY_SMILES(test_A_smiles,\
+                     test_B_smiles,\
+                     training_dataset):
 
-    context = f"A: {A}\
-                B: {B}\
-                Y1:\
-                Y2:\
-                Y3:\
-                Y4:\
-                Y5:\
-                "
+    test = f"A: {test_A_smiles}\
+             B: {test_B_smiles}"
+    answer_format = "{Y1:y1, Y2:y2, Y3:y3, Y4:y4, Y5:y5}"
+    question = "Answer 5 candidates of 'y1 to y5' in \
+                {answer_format}"
+    condition_1 = "Don't use '\n' in your ansewer" #これは微妙
+    condition_2 = "Exclude unclosed ring structures from y1-y5 \
+                    (example:'C1=CC=C(CNC2=NC=C(C=C2)C(=O)N)C(F)(F)F')." #これはなし
+    condition_3 = f"If {test_A_smiles} and {test_B_smiles} are in {training_dataset}, \
+                    the corresponding 'Y' should be included in y1-y5." #これは効果あった
+
+    # エラーになった例を貯めていって学習させるっていうのはあり?
 
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-16k",
     messages=[
-        #{"role": "system", "content": "Synthesize compound Y from test data compounds A and B."},
-        {"role": "assistant", "content": f"{training_dataset}+\n\+{context}"},
-        {"role": "user", "content": f"Answer at least five candidates for 'y1 to y5'."}],
+        {"role": "system", "content": "Synthesize compound Y from test data compounds A and B."},
+        {"role": "assistant", "content": f"{training_dataset}+{test}"},
+        {"role": "user", "content": f"{question}"},
+        {"role": "assistant", "content": f"{condition_1}"},
+        {"role": "assistant", "content": f"{condition_2}"},
+        {"role": "assistant", "content": f"{condition_3}"}
+        ],
         max_tokens=300,
         temperature=0,
         )
