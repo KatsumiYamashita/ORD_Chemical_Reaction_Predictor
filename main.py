0540ac4fa2f85d11ec5d0c25ab612077bbefe84e
KatsumiYamashita <qwerty681104@gmail.com>
2023-06-19 18:47:40 +0900

    予測のモデルをgpt-3.5-turboに変更する

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import openai
+import re
 
 def get_prodY_SMILES(A: str, B: str, training_dataset):
 
-    context =f"{training_dataset}\
-            test data:\
-            {A} + {B} = '?'\
-             "
+    context = f"A: {A}\
+                B: {B}\
+                Y1:\
+                Y2:\
+                Y3:\
+                Y4:\
+                Y5:\
+                "
 
-    question = "Answer a candidate for '?'"
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Question answering:\nContext: {context}\nQuestion: {question}",
-        max_tokens=50)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k",
+    messages=[
+        #{"role": "system", "content": "Synthesize compound Y from test data compounds A and B."},
+        {"role": "assistant", "content": f"{training_dataset}+\n\+{context}"},
+        {"role": "user", "content": f"Answer at least five candidates for 'y1 to y5'."}],
+        max_tokens=300,
+        temperature=0,
+        )
     
-    product_Y = response.choices[0].text.strip()
-    return product_Y
+    response_text = response["choices"][0]["message"]["content"]
+    pattern = r":\s(.+)" #テンプレートの作成
+    product_Y_candidates = re.findall(pattern, response_text) #テンプレートをもとにSMILESを抜き出す
+    return product_Y_candidates
